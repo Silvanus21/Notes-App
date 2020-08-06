740379476854d08a588abc3acbf1acea2e2aed5d
Silvanus <selvasilvanus@gmail.com>
2020-08-06 22:32:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Notes App
+
+[<img src="./Sample-images/sample2.PNG" width=250>](./Sample-images/sample2.PNG) &nbsp;&nbsp;&nbsp;&nbsp; [<img src="./Sample-images/sample1.PNG" width=280>](./Sample-images/sample1.PNG)
+
+---
+
+### Get Started
+
+#### `yarn install`
+
+### Available Scripts
+
+In the project directory, you can run:
+
+#### `yarn start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+#### `yarn test`
+
+Launches the test runner in the interactive watch mode.<br />
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+#### `yarn build`
+
+Builds the app for production to the `build` folder.<br />
+It correctly bundles React in production mode and optimizes the build for the best performance.
---
+++ src/components/App.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [noteArray, setNoteArray] = useState([]);
+
+  function addNote(note) {
+    if (note.title.trim() !== "" && note.content.trim() !== "") {
+      setNoteArray((preValue) => {
+        return [...preValue, note];
+      });
+    } else {
+      alert("Enter Title and Content.");
+    }
+  }
+
+  function deleteNote(id) {
+    setNoteArray((preValue) => {
+      return preValue.filter((eachNote, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="Container">
+      <Header />
+
+      <CreateArea newNote={addNote} />
+
+      <div className="notes-container">
+      {noteArray.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            delete={deleteNote}
+          />
+        );
+      })}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
